f88c4627f0f713484f33d12517bc62e4ce95cc28
Jeremy Chen <jeremy886@gmail.com>
2016-10-25 10:00:18 +1100

    add js to first tile

---
+++ js/familyfeud.js
@@ -0,0 +1,9 @@
+function tile01() {
+    alert("hi");
+    "use strict";
+    var answer01;
+    var elmt;
+    answer01 = "Flappy Bird - 35%";
+    elmt = document.getElementById("answer01");
+    elmt.innerHTML = answer01;
+}
