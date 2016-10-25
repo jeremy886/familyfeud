39e5ff0d09acf09b3fbf871ced9038dec77f6b71
Jeremy Chen <jeremy886@gmail.com>
2016-10-25 15:15:27 +1100

    add use functionalities

--- js/familyfeud.js
+++ js/familyfeud.js
@@ -1,9 +1,17 @@
-function tile01() {
-    alert("hi");
+
+function showCross(id) {
     "use strict";
+    var elmt = document.getElementById(id);
+    elmt.style.transform = "scale(2)";
+}
+
+function tile01() {
+    "use strict";
+    //alert("hi");
     var answer01;
     var elmt;
     answer01 = "Flappy Bird - 35%";
     elmt = document.getElementById("answer01");
     elmt.innerHTML = answer01;
 }
+
