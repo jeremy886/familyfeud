865eaad3f395ace00dc5190ef0f9563c28e7a56c
Jeremy Chen <jeremy886@gmail.com>
2016-10-25 14:46:54 +1100

    showing answers and error crosses

--- js/familyfeud.js
+++ js/familyfeud.js
@@ -1,9 +1,37 @@
-function tile01() {
-    alert("hi");
+function showCross(id) {
     "use strict";
-    var answer01;
+    var elmt = document.getElementById(id);
+    elmt.style.transform = "scale(2)";
+}
+
+function showAnswer(clicked_id) {
+    "use strict";
+    //alert(clicked_id);
     var elmt;
-    answer01 = "Flappy Bird - 35%";
-    elmt = document.getElementById("answer01");
-    elmt.innerHTML = answer01;
+    var answers = [
+        "Flappy Bird - 35%",
+        "Clash of clans - 20%",
+        "Clash Royale - 15 %",
+        "Minecraft - 8%",
+        "Terraria - 5%",
+        "Heads Up! - 3%",
+        "Mario Bro - 1%",
+        "KingKang - 1%",
+        "Snake - 1%",
+        "Minesweeper - 1%"
+    ];
+    var order = clicked_id.slice(-2);
+    if (order.slice(0, 1) === "0") {
+        order = clicked_id.slice(-1);
+    }
+
+    elmt = document.getElementById(clicked_id);
+    
+    //alert(answers[parseInt(order, 10) - 1]);
+    var answer = answers[parseInt(order, 10) - 1];
+    if (typeof answer === "undefined") {
+        answer = "";
+    }
+    elmt.innerHTML = answer;
+    
 }
